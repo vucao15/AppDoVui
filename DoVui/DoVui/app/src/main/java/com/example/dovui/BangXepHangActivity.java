b22763d1dcc8dce69a006e0a03f5a3c80e2add34
vucao15 <vucvph12356@fpt.edu.vn>
2020-11-25 07:47:12 +0700

    first commit

---
+++ DoVui/DoVui/app/src/main/java/com/example/dovui/BangXepHangActivity.java
@@ -0,0 +1,21 @@
+package com.example.dovui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BangXepHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bang_xep_hang);
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+    }
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
+}
